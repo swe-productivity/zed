55078d3ad41ecbff929cbd11c4683db4f14eae98
- <keule_minzig.7o@icloud.com>
2025-12-02 19:53:06 +0100

    fix clipboard MIME type

--- crates/gpui/src/platform/linux/wayland/clipboard.rs
+++ crates/gpui/src/platform/linux/wayland/clipboard.rs
@@ -16,8 +16,8 @@ use crate::{
 };
 
 /// Text mime types that we'll offer to other programs.
-pub(crate) const TEXT_MIME_TYPES: [&str; 3] =
-    ["text/plain;charset=utf-8", "UTF8_STRING", "text/plain"];
+pub(crate) const TEXT_MIME_TYPES: [&str; 2] =
+    ["text/plain;charset=utf-8", "UTF8_STRING"];
 pub(crate) const FILE_LIST_MIME_TYPE: &str = "text/uri-list";
 
 /// Text mime types that we'll accept from other programs.
